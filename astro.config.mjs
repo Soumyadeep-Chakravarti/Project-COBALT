f0561afd13ef8a3df75c59b09808674c6d58bc78
Soumyadeep Chakravarti <soumyadeepsai1@gmail.com>
2026-04-11 22:38:45 +0530

    update blogs

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,7 +19,7 @@ function remarkMermaid() {
 
 export default defineConfig({
   // Set to "/" for root domain deployment (https://soumyadeep-chakravarti.github.io/)
-  base: "/",
+  base: "/Project-COBALT",
 
   // Updated to your GitHub Pages URL
   site: "https://soumyadeep-chakravarti.github.io",
